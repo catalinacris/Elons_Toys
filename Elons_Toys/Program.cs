6bd25e862dba736e619d4a9960f58b35e9992b40
catalinacris <catalinacucos14@yahoo.com>
2022-05-01 05:35:13 +0200

    Add project files.

---
+++ Elons_Toys/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Elons_Toys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemoteControlCar car = new RemoteControlCar();
+            car.BatteryDisplay();
+        }
+    }
+}
---
+++ Elons_Toys/RemoteControlCar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elons_Toys
+{
+    class RemoteControlCar
+    {
+        private int distanceDriven = 0;
+        private int batteryPercentage = 100;
+        public static RemoteControlCar Buy() => new RemoteControlCar();
+
+        public string DistanceDisplay() => $"Driven {distanceDriven} meters";
+
+        public string BatteryDisplay() => 
+            batteryPercentage == 0 ?
+            "Battery empty":
+            $"Battery at {batteryPercentage}%";
+
+        public void Drive()
+        {
+            if(batteryPercentage > 0)
+            {
+                distanceDriven += 20;
+                batteryPercentage--;
+            }
+        }
+    }
+}
